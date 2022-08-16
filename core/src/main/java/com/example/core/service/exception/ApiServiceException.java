8b890d6956f6697aa32a807ec96db5de1a02c167
DimitkoD <84143276+DimitkoD@users.noreply.github.com>
2022-08-16 16:45:25 +0300

    HttpStatusExceptionAdded

--- core/src/main/java/com/example/core/service/exception/ApiServiceException.java
+++ core/src/main/java/com/example/core/service/exception/ApiServiceException.java
@@ -1,4 +1,8 @@
 package com.example.core.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Car not found")
 public class ApiServiceException extends RuntimeException{
 }
